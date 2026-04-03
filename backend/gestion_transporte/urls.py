99d30dfce65339710b763e0f66f665b0c4dd4052
FeRx666g <fer1567g@gmail.com>
2026-04-02 20:58:02 -0500

    Forzar entrega de imagenes en produccion para la tesis

--- backend/gestion_transporte/urls.py
+++ backend/gestion_transporte/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.conf import settings  
 from django.conf.urls.static import static
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,7 +14,6 @@ urlpatterns = [
     path('api/vehiculos/', include('gestion_vehiculos.urls')), 
     path('api/reportes/', include('reportes.urls')),
     path('api/finanzas/', include('gestion_finanzas.urls')),
+    # Agregado para servir imagenes forzosamente en Produccion para la Tesis
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
